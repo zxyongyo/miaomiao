9a94ffab40e19046d9d6311e589eb6357116cde3
zxy <1203123788@qq.com>
2019-09-10 08:59:26 +0800

    p24 over 细节补充

--- src/views/Movie/detail.vue
+++ src/views/Movie/detail.vue
@@ -41,7 +41,7 @@
 			</div> -->
 
 			<div class="detail_list">
-				<div class="detail_list_bg"></div>
+				<div class="detail_list_bg" :style="{ 'background-image':'url('+detailMovie.img.replace(/w\.h/, 148.208)+')'}"></div>
 				<div class="detail_list_filter"></div>
 				<div class="detail_list_content">
 					<div class="detail_list_img">
@@ -122,7 +122,7 @@ export default {
 }
 #content.contentDetail{ display: block; margin-bottom:0;}
 #content .detail_list{ height:200px; width:100%; position: relative; overflow: hidden;}
-.detail_list .detail_list_bg{ width:100%; height:100%; background: url(/images/movie_1.jpg) 0 40%; filter: blur(20px); background-size:cover; position: absolute; left: 0; top: 0;}
+.detail_list .detail_list_bg{ width:100%; height:100%; background: 0 40%; filter: blur(20px); background-size:cover; position: absolute; left: 0; top: 0;}
 .detail_list .detail_list_filter{ width:100%; height:100%; position: absolute;background-color: #40454d;opacity: .55; position: absolute; left: 0; top: 0; z-index: 1;}
 .detail_list .detail_list_content{ display: flex; width:100%; height:100%; position: absolute; left: 0; top: 0; z-index: 2;}
 .detail_list .detail_list_img{ width:108px; height: 150px; border: solid 1px #f0f2f3; margin:20px;}
@@ -131,9 +131,9 @@ export default {
 .detail_list .detail_list_info h2{ font-size: 20px; color:white; font-weight: normal; line-height: 40px;}
 .detail_list .detail_list_info p{ color:white; line-height: 20px; font-size: 14px; color:#ccc;}
 
-#content .detail_intro{ padding: 10px;}
+#content .detail_intro{ padding: 10px; text-indent: 2em;} 
 #content .detail_player{ margin:20px;}
 .detail_player .swiper-slide{ width:70px; margin-right: 20px; text-align: center; font-size: 14px;}
-.detail_player .swiper-slide img{ width:100%; margin-bottom: 5px;}
+.detail_player .swiper-slide img{ width:100%; height: 40px; max-height: 50px; margin-bottom: 5px;}
 .detail_player .swiper-slide p:nth-of-type(2){ color:#999;}
 </style>
